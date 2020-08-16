7c2cfad8e60028f9bbe0033b114d27d01410e12f
Jesse Wayde Brandão <jesse.brandao.91@gmail.com>
2020-08-17 00:10:47 +0200

    Update Authors, version and some other files

--- build.bat
+++
@@ -1,5 +0,0 @@
-cmake . -Bbuild -G"Visual Studio 14 2015"
-cmake --build build --config Release
-
-cmake . -Bbuild64 -G"Visual Studio 14 2015 Win64"
-cmake --build build64 --config Release --target PIMETextService
